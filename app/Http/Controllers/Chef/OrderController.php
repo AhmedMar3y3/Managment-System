d245d75f09b5775782f6d6dfba721d54137280e0
ahmedmar3y1 <86689481+ahmedmar3y1@users.noreply.github.com>
2025-01-08 16:30:47 +0200

    Order from chef prespective

--- app/Http/Controllers/Chef/OrderController.php
+++ app/Http/Controllers/Chef/OrderController.php
@@ -7,22 +7,26 @@ use App\Models\Order;
 use App\Models\Manager;
 use App\Notifications\OrderAccepted;
 use App\Notifications\orderAcceptedByChef;
+use App\Notifications\orderDeclined;
+use App\Notifications\orderDone;
 use Illuminate\Http\Request;
 
 class OrderController extends Controller
 {
     public function index()
     {
-        $orders = Order::where('chef_id', Auth('chef')->id())->get();
+        $orders = Order::where('chef_id', Auth('chef')->id())->get(['order_type', 'order_details', 'delivery_date', 'price']);
         return response()->json(['orders' => $orders], 200);
     }
 
-    public function show($id)
+    //TODO : alter this to get only needed data with images (select is not working with WITH)
+    public function getOrderDetails($id)
     {
-        $order = Order::where('chef_id', Auth('chef')->id())->findOrFail($id)->load('images');
+        $order = Order::where('chef_id', Auth('chef')->id())
+            ->with('Images')
+            ->findOrFail($id, ['order_type', 'order_details', 'delivery_date']);
         return response()->json(['order' => $order], 200);
     }
-
     public function completedOrders()
     {
         $orders = Order::where('chef_id', Auth('chef')->id())->where('status', 'تم التجهيز')->get();
@@ -31,55 +35,76 @@ class OrderController extends Controller
 
     public function newOrders()
     {
-        $orders = Order::where('chef_id', Auth('chef')->id())->where('status', 'جاري الاستلام')->get();
+        $orders = Order::where('chef_id', Auth('chef')->id())->where('status', 'تم القبول')->get();
         return response()->json(['orders' => $orders], 200);
     }
 
     public function acceptOrder($id)
     {
         $order = Order::where('chef_id', Auth('chef')->id())->findOrFail($id);
-        if ($order->status == 'تم القبول') {
-            return response()->json(['message' => 'تم تحديث حالة الطلب مسبقاً'], 200);
-        }
-        $order->status = 'تم القبول';
-        $order->save();
+        if ($order->status == 'وافق المدير') {
+            $order->status = 'تم القبول';
+            $order->save();
 
-        $managerId = $order->manager_id;
-        $manager =Manager::where('id', $managerId)->first();
-        $manager->notify(new orderAcceptedByChef($order));
-        return response()->json(['message' => 'تم تحديث حالة الطلب'], 200);
+            $managerId = $order->manager_id;
+            $manager = Manager::where('id', $managerId)->first();
+            $manager->notify(new orderAcceptedByChef($order));
+            return response()->json(['message' => 'تم تحديث حالة الطلب'], 200);
+        }
+        return response()->json(['message' => 'تم تحديث حالة الطلب مسبقاً'], 200);
     }
 
     public function declineOrder($id)
     {
         $order = Order::where('chef_id', Auth('chef')->id())->findOrFail($id);
-        if ($order->status == 'تم الرفض') {
-            return response()->json(['message' => 'تم تحديث حالة الطلب مسبقاً'], 200);
+        if ($order->status == 'وافق المدير') {
+            $order->status = 'تم الرفض';
+            $order->save();
+            $managerId = $order->manager_id;
+            $manager = Manager::where('id', $managerId)->first();
+            $manager->notify(new orderDeclined($order));
+            return response()->json(['message' => 'تم تحديث حالة الطلب'], 200);
         }
-        $order->status = 'تم الرفض';
-        $order->save();
-        return response()->json(['message' => 'تم تحديث حالة الطلب'], 200);
+        return response()->json(['message' => 'تم تحديث حالة الطلب مسبقاً'], 200);
+
     }
 
     public function orderInProgress($id)
     {
         $order = Order::where('chef_id', Auth('chef')->id())->findOrFail($id);
-        if ($order->status == 'قيد التنفيذ') {
-            return response()->json(['message' => 'تم تحديث حالة الطلب مسبقاً'], 200);
+        if ($order->status == 'تم القبول') {
+            $order->status = 'قيد التنفيذ';
+            $order->save();
+            return response()->json(['message' => 'تم تحديث حالة الطلب', 'status' => 1], 200);
         }
-        $order->status = 'قيد التنفيذ';
-        $order->save();
-        return response()->json(['message' => 'تم تحديث حالة الطلب'], 200);
+        return response()->json(['message' => 'تم تحديث حالة الطلب مسبقاً', 'status' => 0], 200);
+
     }
 
     public function orderDone($id)
     {
         $order = Order::where('chef_id', Auth('chef')->id())->findOrFail($id);
-        if ($order->status == 'تم التجهيز') {
-            return response()->json(['message' => 'تم تحديث حالة الطلب مسبقاً'], 200);
+        if ($order->status == 'قيد التنفيذ') {
+            $order->status = 'تم التجهيز';
+            $order->save();
+
+            $managerId = $order->manager_id;
+            $manager = Manager::where('id', $managerId)->first();
+            $manager->notify(new orderDone($order));
+            return response()->json(['message' => 'تم تحديث حالة الطلب', 'status' => 1], 200);
         }
-        $order->status = 'تم التجهيز';
-        $order->save();
-        return response()->json(['message' => 'تم تحديث حالة الطلب'], 200);
+        return response()->json(['message' => 'تم تحديث حالة الطلب مسبقاً', 'status' => 0], 200);
+
     }
+
+    // public function submitProblem(){
+    //     $data = request()->validate([
+    //         'problem' => 'required',
+    //         'order_id' => 'required',
+    //     ]);
+    //     $order = Order::where('chef_id', Auth('chef')->id())->findOrFail($data['order_id']);
+    //     $order->problem = $data['problem'];
+    //     $order->save();
+    //     return response()->json(['message' => 'تم تقديم الشكوى'], 200);
+    // }
 }
