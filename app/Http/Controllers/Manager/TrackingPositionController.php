79e2b0caa782c4b2fc190daf25a883c32ce2a1c9
zaynab10o <zaynabm766@gmail.com>
2025-01-22 11:40:49 +0200

    jjjjjjj

---
+++ app/Http/Controllers/Manager/TrackingPositionController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\Manager;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Tracking;
+
+class TrackingPositionController extends Controller
+{
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'device_id' => 'required',
+            'latitude' => 'required|numeric',
+            'longitude' => 'required|numeric',
+        ]);
+        $position = Tracking::create($validatedData);
+        return response()->json($position);
+    }
+    
+    public function latest($deviceId)
+    {
+        $position = Tracking::where('device_id', $deviceId)
+            ->latest()
+            ->first();
+        return response()->json($position);
+    }     
+    
+}
---
+++ app/Models/Tracking.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Tracking extends Model
+{
+    use HasFactory;
+    protected $table = 'tracking__positions';
+    protected $fillable=['device_id','latitude','longitude'];
+}
