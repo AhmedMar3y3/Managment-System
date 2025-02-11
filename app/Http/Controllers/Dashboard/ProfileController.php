4021f0b2921b1f1e1d02bf6b0d80c515d400d40f
Mar3y <ahmed.mariee.2002@gmail.com>
2025-02-11 16:29:56 +0200

    done

--- app/Http/Controllers/Dashboard/ProfileController.php
+++ app/Http/Controllers/Dashboard/ProfileController.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Hash;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\admin\UpdateProfile;
 use App\Http\Requests\chef\ChangePasswordRequest;
+use App\Models\User;
 
 class ProfileController extends Controller
 {
@@ -25,6 +26,8 @@ class ProfileController extends Controller
     public function changePassword(ChangePasswordRequest $request)
     {
         $admin = auth()->user();
+        $admin = User::find($admin->id);
+
         if (Hash::check($request->old_password, $admin->password)) {
             $admin->update(['password' => Hash::make($request->new_password)]);
             return response()->json(['message' => 'تم تغيير كلمة المرور بنجاح'], 200);
